7bc938b045cb5f385d6beceadc8d212d7c43ca82
hengkysuryaa <hengkysurya88@gmail.com>
2019-11-18 21:29:38 +0700

    Update modifier.pl

--- modifier.pl
+++ modifier.pl
@@ -75,6 +75,78 @@ modifier(X,Y) :-
 	nattack(Y,W),
 	
 	(
+		myType(A), A == blast, enemyType(B), B == fire,
+		Z1 is Z*1.5,Z2 is W*0.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == blast, enemyType(B), B == water,
+		Z1 is Z*1.5,Z2 is W*0.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == light, enemyType(B), B == fire,
+		Z1 is Z*1.5,Z2 is W*0.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == light, enemyType(B), B == leaves,
+		Z1 is Z*1.5,Z2 is W*0.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == thunder, enemyType(B), B == fire,
+		Z1 is Z*1.5,Z2 is W*0.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == thunder, enemyType(B), B == leaves,
+		Z1 is Z*1.5,Z2 is W*0.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
 		myType(A), A == fire, enemyType(B), B == water,
 		Z1 is Z*0.5,Z2 is W*1.5,
 		retract(myAttack(_)),asserta(myAttack(Z1)),
@@ -137,6 +209,114 @@ modifier(X,Y) :-
 	(
 		myType(A), A == blast, enemyType(B), B == thunder,
 		Z1 is Z*0.5,Z2 is W*1.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == blast, enemyType(B), B == leaves,
+		Z1 is Z*0.5,Z2 is W*1.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == light, enemyType(B), B == water,
+		Z1 is Z*0.5,Z2 is W*1.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == thunder, enemyType(B), B == fire,
+		Z1 is Z*0.5,Z2 is W*1.5,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == fire, enemyType(B), B == fire,
+		Z1 is Z, Z2 is W,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == water, enemyType(B), B == water,
+		Z1 is Z, Z2 is W,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == leaves, enemyType(B), B == leaves,
+		Z1 is Z, Z2 is W,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == blast, enemyType(B), B == blast,
+		Z1 is Z, Z2 is W,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == light, enemyType(B), B == light,
+		Z1 is Z, Z2 is W,
+		retract(myAttack(_)),asserta(myAttack(Z1)),
+		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
+
+	),!.
+
+modifier(X,Y) :-
+	nattack(X,Z), /* X itu myName , Y itu enemyName */
+	nattack(Y,W),
+	
+	(
+		myType(A), A == thunder, enemyType(B), B == thunder,
+		Z1 is Z, Z2 is W,
 		retract(myAttack(_)),asserta(myAttack(Z1)),
 		retract(enemyAttack(_)),asserta(enemyAttack(Z2))
 
@@ -193,6 +373,55 @@ modifierS(X) :-
 modifierS(X) :-
 	sattack(X,Z), /* X itu myName */
 	(
+		myType(A), A == blast, enemyType(B), B == fire,
+		Z1 is Z*1.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == water, enemyType(B), B == water,
+		Z1 is Z*1.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == light, enemyType(B), B == fire,
+		Z1 is Z*1.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == light, enemyType(B), B == leaves,
+		Z1 is Z*1.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == thunder, enemyType(B), B == water,
+		Z1 is Z*1.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == thunder, enemyType(B), B == leaves,
+		Z1 is Z*1.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
 		myType(A), A == fire, enemyType(B), B == water,
 		Z1 is Z*0.5,
 		retract(mySAttack(_)),asserta(mySAttack(Z1))
@@ -235,5 +464,69 @@ modifierS(X) :-
 	(
 		myType(A), A == blast, enemyType(B), B == thunder,
 		Z1 is Z*0.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == blast, enemyType(B), B == leaves,
+		Z1 is Z*0.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == light, enemyType(B), B == water,
+		Z1 is Z*0.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == thunder, enemyType(B), B == fire,
+		Z1 is Z*0.5,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == fire, enemyType(B), B == fire,
+		Z1 is Z,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == water, enemyType(B), B == water,
+		Z1 is Z,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == leaves, enemyType(B), B == leaves,
+		Z1 is Z,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == light, enemyType(B), B == light,
+		Z1 is Z,
+		retract(mySAttack(_)),asserta(mySAttack(Z1))
+	),!.
+
+modifierS(X) :-
+	sattack(X,Z), /* X itu myName */
+	(
+		myType(A), A == thunder, enemyType(B), B == thunder,
+		Z1 is Z,
 		retract(mySAttack(_)),asserta(mySAttack(Z1))
 	),!.
